898290a0488388155c848f129927951d82fd777e
harsha <harshatorgal.99@gmail.com>
2026-07-28 19:54:24 +0530

    commit

--- src/test/java/ParaBank/TestClass.java
+++ src/test/java/ParaBank/TestClass.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -99,9 +100,9 @@ public class TestClass {
         loan.setRequestLoan("2000", "100", 0);
     }
 
-   /* @AfterTest
+    @AfterTest
     public void quit() {
         driver.quit();
-    }*/
+    }
 
 }
