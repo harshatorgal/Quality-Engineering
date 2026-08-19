8d1a719b33d52b395cd6df488b8aee77a42078d7
harsha <harshatorgal.99@gmail.com>
2026-08-19 18:48:43 +0530

    commit

--- src/main/java/lowestAge.java
+++ src/main/java/lowestAge.java
@@ -1,5 +1,5 @@
 public class lowestAge {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         int a[] = {78, 34, 66, 90, 23, 4};
         int lowestAge = a[0];
         for (int i = 0; i < a.length; i++) {
@@ -9,5 +9,25 @@ public class lowestAge {
 
         }
         System.out.println(lowestAge);
+    }*/
+
+
+    public static void main(String[] args) {
+        int a[] = {78, 34, 66, 90, 23, 4};
+        int highestAge = a[0];
+        int lowestAge = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (highestAge < a[i]) {
+                highestAge = a[i];
+            }
+            if (lowestAge > a[i]) {
+                lowestAge = a[i];
+            }
+        }
+        System.out.println("Highest age: " + highestAge);
+        System.out.println("Lowest age: " + lowestAge);
+
     }
 }
+
+
