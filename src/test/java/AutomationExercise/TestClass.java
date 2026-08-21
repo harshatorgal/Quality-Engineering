258b6216664294619e4e46e7b4e7e79a9132cb77
harsha <harshatorgal.99@gmail.com>
2026-08-21 18:40:13 +0530

    commit

--- src/test/java/AutomationExercise/TestClass.java
+++ src/test/java/AutomationExercise/TestClass.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package AutomationExercise;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,102 +16,101 @@ import org.testng.annotations.Test;
 public class TestClass {
     WebDriver driver;
 
+    public TestClass() {
+    }
+
     @BeforeTest
     public void Test1() {
         WebDriverManager.chromedriver().setup();
-
-        driver = new ChromeDriver();
-        driver.get("https://automationexercise.com/login");
-        driver.manage().window().maximize();
-
+        this.driver = new ChromeDriver();
+        this.driver.get("https://automationexercise.com/login");
+        this.driver.manage().window().maximize();
     }
 
-    @Test(priority = 0)
+    @Test(
+            priority = 0
+    )
     public void SignUpPage() {
-        SignUpPage signup = new SignUpPage(driver);
+        SignUpPage signup = new SignUpPage(this.driver);
         signup.setSignUpPage("AbD", "ABD@gmail.com");
         signup.setDetails("1234567890", "3", "June", 2, "ABD", "DD", "qwerr", 1, "ASD", "AWE", "123456", "2345543223");
-
     }
 
-    @Test(priority = 1)
+    @Test(
+            priority = 1
+    )
     public void InvalidLoginPage() {
-
-        LoginPage login = new LoginPage(driver);
+        LoginPage login = new LoginPage(this.driver);
         login.setLogin("0@gmail.com", "10");
         login.getErrorMessage();
     }
 
-    @Test(priority = 2)
+    @Test(
+            priority = 2
+    )
     public void ValidLoginPage() {
-
-        LoginPage login = new LoginPage(driver);
+        LoginPage login = new LoginPage(this.driver);
         login.setLogin("ABD@gmail.com", "1234567890");
         login.getErrorMessage();
     }
 
-    @Test(priority = 3)
+    @Test(
+            priority = 3
+    )
     public void HomePage() {
-        HomePage home = new HomePage(driver);
+        HomePage home = new HomePage(this.driver);
         home.setHomePage1();
-
         home.setHomePage2();
     }
 
-    @Test(priority = 4)
+    @Test(
+            priority = 4
+    )
     public void ProductPage() {
-        ProductPage product = new ProductPage(driver);
-
-        HomePage home = new HomePage(driver);
+        ProductPage product = new ProductPage(this.driver);
+        HomePage home = new HomePage(this.driver);
         String price1 = home.getItemPrice();
-
         product.setProductPage1();
         String price2 = product.getItemPrice();
         product.setProductPage2();
-
         if (price1 == price2) {
             System.out.println("Prices are matching");
         } else {
             System.out.println("Prices are not matching");
         }
+
     }
 
-    @Test(priority = 5)
+    @Test(
+            priority = 5
+    )
     public void CartCheckoutPage() {
-        CartPage cart = new CartPage(driver);
-        CheckoutPage checkout = new CheckoutPage(driver);
-
+        CartPage cart = new CartPage(this.driver);
+        CheckoutPage checkout = new CheckoutPage(this.driver);
         String firstPrice = cart.getFirstPrice();
         double fprice = Double.parseDouble(firstPrice.replace("Rs. ", ""));
-
         String secondPrice = cart.getSecondPrice();
         double sPrice = Double.parseDouble(secondPrice.replace("Rs. ", ""));
-
         double total = fprice + sPrice;
         cart.setCartPage();
-
         double totalPrice = checkout.getTotalPrice();
-
         Assert.assertEquals(total, totalPrice, "Total price is correct");
-
         String deliveryAddress = checkout.getDAddress();
         String billingAddress = checkout.getBAddress();
-
         Assert.assertEquals(deliveryAddress, billingAddress, "Address are correct");
-
         checkout.setCheckoutPage("Good");
-
     }
 
-    @Test(priority = 6)
+    @Test(
+            priority = 6
+    )
     public void PaymentPage() {
-        PaymentPage payment = new PaymentPage(driver);
+        PaymentPage payment = new PaymentPage(this.driver);
         payment.setPaymentPage("ASDD", "1234567887654321", "123", "02", "2030");
     }
 
     @AfterTest
     public void quit() {
-        driver.quit();
+        this.driver.quit();
     }
-
 }
