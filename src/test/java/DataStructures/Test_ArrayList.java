fcd1526daaa83a810566a9c320331bf1bb4e4daa
harsha <harshatorgal.99@gmail.com>
2026-09-08 22:34:54 +0530

    commit

--- src/test/java/DataStructures/Test_ArrayList.java
+++ src/test/java/DataStructures/Test_ArrayList.java
@@ -1,6 +1,7 @@
 package DataStructures;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Test_ArrayList {
     public static void main(String[] args) {
@@ -10,7 +11,20 @@ public class Test_ArrayList {
         city.add("Christchurch");
         city.add("Dunedin");
         city.add("Dunedin");
+        city.add("Bangalore");
         System.out.println("Some major cities of NewZealand are:");
+        System.out.println(city);
+
+        Iterator<String> it = city.iterator();
+        while (it.hasNext()) {
+
+            String currentcity = it.next();
+            System.out.println(currentcity);
+            if (currentcity.equals("Bangalore")) {
+                it.remove();
+            }
+
+        }
         System.out.println(city);
 
     }
