63285e02f84530aa47ddd653eb146a840a97c8a2
harsha <harshatorgal.99@gmail.com>
2026-09-08 20:46:59 +0530

    commit

---
+++ src/test/java/DataStructures/Test_ArrayList.java
@@ -0,0 +1,16 @@
+package DataStructures;
+
+import java.util.ArrayList;
+
+public class Test_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> city = new ArrayList<String>();
+        city.add("Auckland");
+        city.add("Wellington");
+        city.add("Christchurch");
+        city.add("Dunedin");
+        System.out.println("Some major cities of NewZealand are:");
+        System.out.println(city);
+
+    }
+}
