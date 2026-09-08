ac7e4f53c629ce7683b6b4cfcb33cbde83a66474
harsha <harshatorgal.99@gmail.com>
2026-09-08 21:32:22 +0530

    commit

---
+++ src/test/java/DataStructures/Test_LinkedList.java
@@ -0,0 +1,15 @@
+package DataStructures;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test_LinkedList {
+    public static void main(String[] args) {
+        List<String> abc = new LinkedList<String>();
+        abc.add("Java");
+        abc.add("List");
+        System.out.println(abc);
+    }
+
+
+}
